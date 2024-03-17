d524f86f2c7940df557bd99ed4b1d003a63a9c55
BespalovKirill <162878858+BespalovKirill@users.noreply.github.com>
2024-03-17 12:42:41 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <?php
+        $ruble = 81.2;
+        $rubles = 500*$ruble;
+        $yuan = 0.07;
+        $yuans = $yuan * $rubles;
+        echo $yuans." CNY";
+        ?>
+    </body>
+</html>
